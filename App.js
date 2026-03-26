c1669fdca7939acf746184744a238b948238da64
Krishnacodes999-eng <krishnachowdhary734@gmail.com>
2026-03-26 19:57:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// frontend/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Military Asset Management Dashboard</h1>
+      <p>Basic UI Setup</p>
+    </div>
+  );
+}
+
+export default App;
